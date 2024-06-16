f36160889d40ea6ca2dfc977252346a9f847a309
Heidy Nicole Arancibia <arancibiaheidy36@gmail.com>
2024-06-15 21:21:21 -0400

    p4

---
+++ Pregunta4/app.php
@@ -0,0 +1,22 @@
+
+
+<form action="http://localhost:8080/EjerJAVA/MyServlet" method="POST">
+    <label for="nombre">Nombre:</label>
+    <input type="text" id="nombre" name="nombre"><br><br>
+    <label for="edad">Edad:</label>
+    <input type="text" id="edad" name="edad"><br><br>
+    <input type="submit" value="Submit">
+</form>
+
+
+
+
+
+<form action="https://localhost:44365/WebForm1.aspx" method="POST">
+    <label for="nombre">Nombre:</label>
+    <input type="text" id="nombre" name="nombre"><br><br>
+    <label for="edad">Edad:</label>
+    <input type="text" id="edad" name="edad"><br><br>
+    <input type="submit" value="Submit">
+</form>
+
